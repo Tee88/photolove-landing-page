271c634ed776bdba1252bb8f27270f1a8e5eeb27
Taha Saleh <taha.a.saleh@gmail.com>
2017-07-29 15:34:33 +0100

    Complete responsive website

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+    
+    $("#nav-mobile").html($(".nav-main").html());
+    $("#nav-trigger img").click(function(){
+        if ($("#nav-mobile ul").hasClass("expanded")) {
+            $("#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+            $(this).removeClass("open");
+        } else {
+            $("#nav-mobile ul").addClass("expanded").slideDown(250);
+            $(this).addClass("open");
+        }
+    });
+    
+    
+    
     $('.cta-img').waypoint(function(direction){
         $('.cta-img').addClass('animated slideInUp');
         $('.iphone-btn').delay(1500).animate({top: "+=3"},200);
@@ -8,13 +22,34 @@ $(document).ready(function(){
         offset: '70%'
     });
     
-    $('.col-4-app').waypoint(function(direction){
+    
+    if(window.matchMedia('(max-width: 935px)').matches) {
+        $('.scalable').waypoint(function(direction){
         $('.scalable').addClass('animated slideInLeft');
-        $('.instant').addClass('animated slideInUp');
-        $('.something').addClass('animated slideInRight');
     },{
         offset: '70%'
     });
+        $('.instant').waypoint(function(direction){
+        $('.instant').addClass('animated slideInRight');
+    },{
+        offset: '70%'
+    });
+        $('.something').waypoint(function(direction){
+        $('.something').addClass('animated slideInLeft');
+    },{
+        offset: '70%'
+    });
+        }else{
+            
+        $('.col-4-app').waypoint(function(direction){
+            $('.scalable').addClass('animated slideInLeft');
+            $('.instant').addClass('animated slideInUp');
+            $('.something').addClass('animated slideInRight');
+        },{
+            offset: '70%'
+        });
+        }
+    
     
     $('.clown-info').waypoint(function(direction){
         $('.clown-info').addClass('animated fadeIn');
@@ -37,7 +72,7 @@ $(document).ready(function(){
     
     $(".nav li:nth-child(5)").click(function() {
         $('html, body').animate({
-            scrollTop: $(".clown").offset().top+700
+            scrollTop: $("footer").offset().top
         }, 1500);
     });
 });
